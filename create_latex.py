aef60b319a4697d4832d4901be4e12c2a619b2ba
Elena Mazzeo <elena.mazzeo@cern.ch>
2025-01-04 13:21:51 +0100

    add option to change title and add author

--- create_latex.py
+++ create_latex.py
@@ -7,6 +7,9 @@ parser = argparse.ArgumentParser(description='Create pdf with bibliography',
                                  formatter_class=argparse.RawDescriptionHelpFormatter,
                                  epilog='example: create_latex bibtex_2016-02-07.bib')
 parser.add_argument('bibtex')
+parser.add_argument('--title', default='List of publications', help='Title of the document')
+parser.add_argument('--author', default=None, help='Author of the document')
+
 args = parser.parse_args()
 
 TEMPLATE_FILENAME = "template_latex.tex"
@@ -14,6 +17,12 @@ with open(TEMPLATE_FILENAME) as f:
     template = f.read()
 
 template = template.replace("ADD_BIBTEX_HERE", args.bibtex)
+template = template.replace("ADD_TITLE_HERE", args.title)
+if args.author:
+    template = template.replace("ADD_AUTHOR_HERE", args.author)
+else: 
+    template = template.replace("\\author{ADD_AUTHOR_HERE}", "")
+
 with open('publications.tex', 'w') as f:
     f.write(template)
 
